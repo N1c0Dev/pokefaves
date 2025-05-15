0ace208ef365f5c24020b80678c69a72494a0309
Nicodev <nicolasgongar@gmail.com>
2025-05-15 19:46:19 -0400

    add: base welcome view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import WelcomeView from '../views/WelcomeView.vue'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -6,16 +7,13 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'welcome',
+      component: WelcomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/home',
+      name: 'home',
+      component: HomeView,
     },
   ],
 })
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="flex flex-col items-center justify-center h-screen">
+    <img src="@/assets/logo.svg" alt="Pikachu waving">
+    <h1 class="text-3xl font-bold text-center">Welcome to the Pokedex</h1>
+    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, temporibus repellat sequi quibusdam iste labore molestias, tempore deserunt atque aut, nihil enim tenetur illum. A aperiam tempora culpa impedit quam!</p>
+    <button class="mt-4 bg-red-500 text-white py-2 px-4 rounded-full">
+      <router-link to="/home">Get started</router-link>
+    </button>
+  </section>
+</template>
