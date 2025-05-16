6f3315bbcf8942340116d52ee53d272a4f079021
Nicodev <nicolasgongar@gmail.com>
2025-05-16 00:38:34 -0400

    add: base layout

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
 </script>
 
 <template>
-  <main class="debug-screens">
+  <main class="debug-screens flex">
+    <div class="w-1/3"/>
     <RouterView />
+    <div class="w-1/3"/>
   </main>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,23 +5,23 @@ import DetailsModal from '@/components/DetailsModal.vue';
 
 <template>
   <section>
-    <input type="text" placeholder="Search" class="bg-white border p-2 w-full mb-4" />
+    <input type="text" placeholder="Search" class="bg-white border p-2 w-full mb-4 font-medium" />
   </section>
   <section>
     <div class="grid grid-cols-1 gap-4">
       <div class="flex justify-between bg-white p-4 border">
-        <h2 class="text-xl font-bold">Pokemon 1</h2>
+        <h2 class="text-[22px] font-medium">Pokemon 1</h2>
         <p>lorem</p>
       </div>
       <div class="flex justify-between bg-white p-4 border">
-        <h2 class="text-xl font-bold">Pokemon 2</h2>
+        <h2 class="text-[22px] font-medium">Pokemon 1</h2>
         <p>lorem</p>
       </div>
     </div>
   </section>
   <DetailsModal/>
   <footer class="flex justify-center shadow-md bg-white rounded-lg p-4 gap-4">
-    <button class="mt-4 bg-red-500 text-white py-2 px-4 rounded-full">All</button>
-    <button class="mt-4 bg-red-500 text-white py-2 px-4 rounded-full">Favorites</button>
+    <button class="mt-4 bg-red-500 text-white py-2 px-4 rounded-full font-bold">All</button>
+    <button class="mt-4 bg-red-500 text-white py-2 px-4 rounded-full font-bold">Favorites</button>
   </footer>
 </template>
