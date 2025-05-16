58de856bb0177c280277429789cb09fcdf9fddba
Nicodev <nicolasgongar@gmail.com>
2025-05-16 16:32:39 -0400

    feat: add loading screen component and integrate it into home view

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  isVisible: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <section v-if="isVisible" class="flex ">
+    <img class="animate-pulse h-[106px] w-[106px] m-auto" src="@/assets/images/pokeball.svg" alt="loading">
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,54 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue';
-import axios from 'axios';
+import { ref, reactive, onMounted } from 'vue'
+import axios from 'axios'
 
-import { debounce } from '@/utils/debounce';
-import DetailsModal from '@/components/DetailsModal.vue';
+import { debounce } from '@/utils/debounce'
+import DetailsModal from '@/components/DetailsModal.vue'
+import LoadingScreen from '@/components/LoadingScreen.vue'
 
-const loading = ref(false);
-const currentPagination = reactive({});
-const pokemonList = ref([]);
+const mountedLoading = ref(false)
+const searchLoading = ref(false)
+const currentPagination = reactive({})
+const pokemonList = ref([])
+
+onMounted(async () => {
+  try {
+    mountedLoading.value = true
+
+    const res = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+    pokemonList.value = res.data.results
+  } catch (err) {
+    console.error(err)
+  } finally {
+    setTimeout(() => {
+      mountedLoading.value = false
+    }, 1500)
+  }
+})
 
 async function handleSearch(search: string) {
-  console.log('search:', search.target.value);
-  loading.value = true
+  console.log('search:', search.target.value)
+  searchLoading.value = true
   try {
     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search.target.value}`)
-    pokemonList.value = search.target.value == '' ? res.data.results : [res.data];
-    console.log('pokemonList:', pokemonList.value);
+    pokemonList.value = search.target.value == '' ? res.data.results : [res.data]
   } catch (err) {
     console.error(err);
   } finally {
-    loading.value = false
+    setTimeout(() => {
+      searchLoading.value = false
+    }, 500)
   }
 }
 
 
-const debounceSearch = debounce(handleSearch, 500);
+const debounceSearch = debounce(handleSearch, 500)
 
 </script>
 
 <template>
-  <section class="flex">
+  <LoadingScreen class="h-screen" :isVisible="mountedLoading" />
+  <section v-if="!mountedLoading" class="flex">
     <div class="w-1/12"/>
     <div class="w-10/12 mt-10">
       <section class="relative mb-4 ">
@@ -43,7 +62,8 @@ const debounceSearch = debounce(handleSearch, 500);
           @keyup="debounceSearch"
         />
       </section>
-      <section class="grid grid-cols-1 gap-4 mt-10 mb-30">
+      <LoadingScreen class="h-50" :isVisible="searchLoading" />
+      <section v-if="!searchLoading" class="grid grid-cols-1 gap-4 mt-10 mb-30">
         <div
           v-for="(item, index) in pokemonList"
           :key="index"
@@ -58,9 +78,7 @@ const debounceSearch = debounce(handleSearch, 500);
     </div>
     <div class="w-1/12"/>
   </section>
-
-
-  <footer class="flex justify-center shadow-[0px_-5px_4px_0px_#0000000D]  bg-white p-4 gap-4 fixed bottom-0 left-0 right-0">
+  <footer v-if="!mountedLoading" class="flex justify-center shadow-[0px_-5px_4px_0px_#0000000D]  bg-white p-4 gap-4 fixed bottom-0 left-0 right-0">
     <button class="h-11 w-[150px] mt-4 bg-primary-button text-white rounded-full font-bold cursor-pointer flex justify-center gap-3">
       <img class="self-center" src="@/assets/images/list-icon.svg" alt="">
       <p class="self-center">All</p>
