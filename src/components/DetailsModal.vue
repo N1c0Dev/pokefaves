8eda08f209167c0abcf24cbf81fa347173c34bc9
Nicodev <nicolasgongar@gmail.com>
2025-05-17 14:56:19 -0400

    refactor: details modal and dots spinner for improved readability and styling

--- src/components/DetailsModal.vue
+++ src/components/DetailsModal.vue
@@ -30,44 +30,197 @@ function addToFavorites() {
   <transition name="fade-modal">
     <div
       v-if="isVisible"
-      class="fixed inset-0 z-50 flex items-center justify-center bg-black/60"
+      class="
+        fixed
+        inset-0
+        z-50
+        flex
+        items-center
+        justify-center
+        bg-black/60"
     >
-      <div class="bg-white rounded-[5px] max-w-md w-8/12 pb-6 relative animate-fade-in">
-        <div class="relative w-full h-64 mb-3 bg-[url('@/assets/images/landscape.svg')] bg-cover bg-center rounded-t-[5px] overflow-hidden">
+      <div
+        class="
+        bg-white
+        rounded-[5px]
+        max-w-md
+        w-8/12
+        pb-6
+        relative
+        animate-fade-in
+      "
+    >
+        <div
+          class="
+            relative
+            w-full
+            h-64
+            mb-3
+            bg-[url('@/assets/images/landscape.svg')]
+            bg-cover
+            bg-center
+            rounded-t-[5px]
+            overflow-hidden
+          "
+          >
           <img
+            class="
+              absolute
+              top-4
+              right-5
+              w-[26px]
+              h-[26px]
+              object-contain
+              z-10
+              cursor-pointer
+            "
             src="@/assets/images/close-icon.svg"
-            class="absolute top-4 right-5 w-[26px] h-[26px] object-contain z-10 cursor-pointer"
             alt="close modal"
             @click="closeFunc"
           />
           <img
+            class="
+              absolute
+              top-[54%]
+              left-1/2
+              transform
+              -translate-x-1/2
+              -translate-y-1/2
+              w-45
+              h-45
+              object-contain
+              z-20
+            "
             :src="pokemonDetails.sprites.other['official-artwork'].front_default || pokemonDetails.sprites.front_default"
-            class="absolute top-[54%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-45 h-45 object-contain z-20"
-            alt=""
+            :alt="pokemonDetails.name"
           />
         </div>
         <section class="px-6">
           <div>
-            <div class="flex bg-white border-b border-primary-border text-secondary-color py-3">
-              <h4 class="text-lg font-bold self-baseline">Name:</h4>
-              <p class="self-baseline ml-1 capitalize">{{ pokemonDetails.name }}</p>
+            <div
+              class="
+                flex
+                bg-white
+                border-b
+                border-primary-border
+                text-secondary-color
+                py-3
+              "
+            >
+              <h4
+                class="
+                  text-lg
+                  font-bold
+                  self-baseline
+                "
+              >
+                Name:
+              </h4>
+              <p
+                class="
+                  self-baseline
+                  ml-1
+                  capitalize
+                "
+              >
+                {{ pokemonDetails.name }}
+              </p>
             </div>
-            <div class="flex bg-white border-b border-primary-border text-secondary-color py-3">
-              <h4 class="text-lg font-bold self-baseline">Weight:</h4>
-              <p class="self-baseline ml-1">{{ pokemonDetails.weight }}</p>
+            <div
+              class="
+                flex
+                bg-white
+                border-b
+                border-primary-border
+                text-secondary-color
+                py-3
+              "
+            >
+              <h4
+                class="
+                  text-lg
+                  font-bold
+                  self-baseline
+                "
+              >
+                Weight:
+              </h4>
+              <p
+                class="
+                  self-baseline
+                  ml-1
+                "
+              >
+                {{ pokemonDetails.weight }}
+              </p>
             </div>
-            <div class="flex bg-white border-b border-primary-border text-secondary-color py-3">
-              <h4 class="text-lg font-bold self-baseline">Height:</h4>
-              <p class="self-baseline ml-1">{{ pokemonDetails.height }}</p>
+            <div
+              class="
+                flex
+                bg-white
+                border-b
+                border-primary-border
+                text-secondary-color
+                py-3
+              "
+            >
+              <h4
+                class="
+                  text-lg
+                  font-bold
+                  self-baseline
+                "
+              >
+                Height:
+              </h4>
+              <p
+                class="
+                  self-baseline
+                  ml-1
+                "
+              >
+                {{ pokemonDetails.height }}
+              </p>
             </div>
-            <div class="flex bg-white border-b border-primary-border text-secondary-color py-3">
-              <h4 class="text-lg font-bold self-baseline">Types:</h4>
-              <p class="self-baseline ml-1 capitalize">{{ typeNames }}</p>
+            <div
+              class="
+                flex
+                bg-white
+                border-b
+                border-primary-border
+                text-secondary-color
+                py-3
+              "
+            >
+              <h4
+                class="
+                  text-lg
+                  font-bold
+                  self-baseline
+                "
+              >
+                Types:
+              </h4>
+              <p
+                class="
+                  self-baseline
+                  ml-1
+                  capitalize
+                "
+              >
+                {{ typeNames }}
+              </p>
             </div>
           </div>
         </section>
         <section class="px-6">
-          <div class="flex justify-between mt-4">
+          <div
+            class="
+              flex
+              justify-between
+              mt-4
+            "
+          >
             <ButtonElement
               class="font-bold"
               :clickFunc="handleShare"
