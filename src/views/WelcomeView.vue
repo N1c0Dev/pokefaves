05e5283488e9b34a7657cab7155d7e7a42dee038
Nicodev <nicolasgongar@gmail.com>
2025-05-18 11:01:58 -0400

    feat: add wave animation to pikachu image in welcome view

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import ButtonElement from '@/components/forms/ButtonElement.vue'
 </script>
+
 <template>
   <section class="flex">
     <div class="w-2/12" />
@@ -15,6 +16,11 @@ import ButtonElement from '@/components/forms/ButtonElement.vue'
       "
     >
       <img
+        class="
+          animate-wave-hello
+          inline-block
+          origin-bottom-left
+        "
         src="@/assets/images/pikachu.svg"
         alt="Pikachu waving"
       >
@@ -58,5 +64,17 @@ import ButtonElement from '@/components/forms/ButtonElement.vue'
     </section>
     <div class="w-2/12" />
   </section>
-
 </template>
+
+<style scoped lang="css">
+@keyframes wave-hello {
+  0% { transform: rotate(0deg); }
+  30% { transform: rotate(3deg); }
+  60% { transform: rotate(-4deg); }
+  100% { transform: rotate(0deg); }
+}
+
+.animate-wave-hello {
+  animation: wave-hello 1s ease-in-out 1;
+}
+</style>
